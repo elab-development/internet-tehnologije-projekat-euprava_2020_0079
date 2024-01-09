7da134450eebaafabb49ae6b64a612ded654b4a6
Kosanovic24 <petar24kosanovic@gmail.com>
2024-01-09 21:35:28 +0100

    kreirani resources za sve modele

---
+++ laravelApp/app/Http/Resources/UserResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'ime' => $this->ime,
+            'prezime' => $this->prezime,
+            'imeOca' => $this->imeOca,
+            'email' => $this->email,
+            'uloga' => $this->uloga,
+            'jmbg' => $this->jmbg,
+            'brLicneKarte' => $this->brLicneKarte,
+            'datumRodjenja' => $this->datumRodjenja,
+            'mestoRodjenja' => $this->mestoRodjenja,
+            'br_lk' => $this->br_lk,
+            'adresaPrebivalista' => $this->adresaPrebivalista,
+            'opstinaPrebivalista' => $this->opstinaPrebivalista, 
+           
+        ];
+    }
+}
