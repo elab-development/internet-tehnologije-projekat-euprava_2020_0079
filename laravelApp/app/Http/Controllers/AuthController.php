d4840ac42f9ec2ff7f74597a39e0cb54ef4dc58b
MihailoStankovic <mihailos02@gmail.com>
2024-01-10 19:54:04 +0100

    popravljena metoda register

--- laravelApp/app/Http/Controllers/AuthController.php
+++ laravelApp/app/Http/Controllers/AuthController.php
@@ -11,27 +11,48 @@ use Illuminate\Support\Facades\Validator;
 class AuthController extends Controller
 {
     public function register(Request $request)
-    {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
-        ]);
+{
+    $validator = Validator::make($request->all(), [
+        'ime' => 'required|string|max:255',
+        'prezime' => 'required|string|max:255',
+        'imeOca' => 'nullable|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:6',
+        'uloga' => 'required|string|max:255',
+        'jmbg' => 'required|string|max:13|unique:users',
+        'brLicneKarte' => 'required|string|max:20|unique:users',
+        'datumRodjenja' => 'required|date',
+        'mestoRodjenja' => 'required|string|max:255',
+        'br_lk' => 'required|string|max:20',
+        'adresaPrebivalista' => 'required|string|max:255',
+        'opstinaPrebivalista' => 'required|string|max:255'
+    ]);
 
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
-
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        return response()->json(['access_token' => $token, 'token_type' => 'Bearer']);
+    if ($validator->fails()) {
+        return response()->json($validator->errors(), 400);
     }
+
+    $user = User::create([
+        'ime' => $request->ime,
+        'prezime' => $request->prezime,
+        'imeOca' => $request->imeOca,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+        'uloga' => $request->uloga,
+        'jmbg' => $request->jmbg,
+        'brLicneKarte' => $request->brLicneKarte,
+        'datumRodjenja' => $request->datumRodjenja,
+        'mestoRodjenja' => $request->mestoRodjenja,
+        'br_lk' => $request->br_lk,
+        'adresaPrebivalista' => $request->adresaPrebivalista,
+        'opstinaPrebivalista' => $request->opstinaPrebivalista
+    ]);
+
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return response()->json(['access_token' => $token, 'token_type' => 'Bearer']);
+}
+
 
     public function login(Request $request)
     {
