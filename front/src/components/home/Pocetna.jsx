08eb5f3f60aee4cb9a74f84bec04e86692430be2
MihailoStankovic <mihailos02@gmail.com>
2024-01-19 22:13:34 +0100

    dodat slajder za slike na pocetnoj stranici

--- front/src/components/home/Pocetna.jsx
+++ front/src/components/home/Pocetna.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Slider from 'react-slick';
 import './Pocetna.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function PocetnaStranica() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    // Here you would fetch images from the Unsplash API
+    // For demonstration, we're using static image URLs.
+    setImages([
+      'https://source.unsplash.com/featured/?school',
+      'https://source.unsplash.com/featured/?kindergarten',
+      'https://source.unsplash.com/featured/?job',
+      'https://source.unsplash.com/featured/?education',
+      'https://source.unsplash.com/featured/?family'
+    ]);
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000
+  };
   return (
     <div className="pocetna">
       <header className="pocetna-header">
@@ -27,6 +53,13 @@ function PocetnaStranica() {
           <p>Najlakši načini do posla.</p>
         </div>
       </section>
+      <Slider {...settings}>
+          {images.map((img, index) => (
+            <div key={index}>
+              <img src={img} alt="Slika" />
+            </div>
+          ))}
+        </Slider>
     </div>
   );
 }
