1556339660e0a701ab6b98edb59753990ae316f1
MihailoStankovic <mihailos02@gmail.com>
2024-01-19 22:34:16 +0100

    dodata reusable komponenta navbar

--- front/src/App.js
+++ front/src/App.js
@@ -3,11 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
  
 import PocetnaStranica from './components/home/Pocetna';
 import Usluge from './components/usluge/Usluge';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar></Navbar>
       <Routes>
+        
         <Route path="/" element={<PocetnaStranica />} />
         <Route path="/usluge" element={<Usluge />} />
       </Routes>
---
+++ front/src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-container">
+        
+        <div className="nav-links">
+          <Link to="/" className="nav-item">Početna</Link>
+          <Link to="/usluge" className="nav-item">Usluge</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
